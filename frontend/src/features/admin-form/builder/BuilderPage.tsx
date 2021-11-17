9cd064532bbc25b94a1f78fab188a595fea4238c
Kar Rui Lau <karrui.lau@gmail.com>
2021-11-17 22:59:22 +0800

    feat: add field creation and field reorder differentiation

--- frontend/src/features/admin-form/builder/BuilderPage.tsx
+++ frontend/src/features/admin-form/builder/BuilderPage.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import { createPortal } from 'react-dom'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import { DndContext, DragOverlay, useDroppable } from '@dnd-kit/core'
 import { arrayMove, SortableContext, useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
+import { omit, pick } from 'lodash'
 
 import { BuilderContent } from './components/BuilderContent'
 import { BuilderDrawer } from './components/BuilderDrawer'
@@ -12,18 +13,24 @@ import { BuilderSidebar } from './components/BuilderSidebar'
 import { generateBasicFieldItems, useBuilderPage } from './BuilderPageContext'
 import { DragItem, FieldDropType } from './types'
 
+const removeIsCreate = (dragItem: DragItem) => {
+  return omit(dragItem, 'isCreate')
+}
+
 export const BuilderPage = (): JSX.Element => {
   const [copyDragItem, setCopyDragItem] = useState<DragItem | null>(null)
   const { draggableBasicFieldItems, setDraggableBasicFieldItems } =
     useBuilderPage()
 
-  const [sortItems, setSortItems] = useState<{ id: string }[]>([])
+  const [sortItems, setSortItems] = useState<DragItem[]>([])
 
   return (
     <DndContext
       onDragStart={({ active }) => {
         if (active.data.current?.type === FieldDropType.Create) {
-          setCopyDragItem(active.data.current as DragItem)
+          setCopyDragItem(
+            pick(active.data.current, ['id', 'fieldType']) as DragItem,
+          )
         }
       }}
       onDragOver={(e) => {
@@ -31,7 +38,7 @@ export const BuilderPage = (): JSX.Element => {
         if (copyDragItem && over?.id === 'sortArea') {
           const sortIndex = sortItems.findIndex((i) => i.id === copyDragItem.id)
           if (sortIndex < 0) {
-            setSortItems([...sortItems, { ...copyDragItem }])
+            setSortItems([...sortItems, { ...copyDragItem, isCreate: true }])
           }
         }
       }}
@@ -53,9 +60,12 @@ export const BuilderPage = (): JSX.Element => {
           }
           const activeIndex = sortItems.findIndex((i) => i.id === active.id)
           const overIndex = sortItems.findIndex((i) => i.id === over.id)
-          if (overIndex !== activeIndex) {
-            setSortItems((items) => arrayMove(items, activeIndex, overIndex))
-          }
+          setSortItems((items) => {
+            const nextSortItems = items.map(removeIsCreate)
+            return overIndex !== activeIndex
+              ? arrayMove(nextSortItems, activeIndex, overIndex)
+              : nextSortItems
+          })
         }
         setCopyDragItem(null)
       }}
@@ -105,22 +115,44 @@ const SortArea = (props: any) => {
   )
 }
 
-const SortItem = ({ item }: any) => {
-  console.log(item)
+const SortItem = ({ item }: { item: DragItem }) => {
   const { listeners, setNodeRef, transform, transition, isDragging } =
     useSortable({
       id: item.id,
       data: {
+        ...item,
         type: FieldDropType.Reorder,
-        item,
       },
     })
 
   const style = {
     transform: CSS.Translate.toString(transform),
     transition,
-    width: 90,
-    background: isDragging ? 'green' : '#ccc',
+  }
+
+  if (isDragging && item.isCreate) {
+    return (
+      <Box
+        style={style}
+        ref={setNodeRef}
+        {...listeners}
+        py="2.25rem"
+        px="2.5rem"
+      >
+        <Box
+          py="1.5rem"
+          px="2.5rem"
+          bg="primary.200"
+          border="1px solid"
+          borderColor="primary.500"
+          textAlign="center"
+        >
+          <Text textStyle="subhead-2" color="primary.400">
+            Drop your field here
+          </Text>
+        </Box>
+      </Box>
+    )
   }
 
   return (
--- frontend/src/features/admin-form/builder/types.ts
+++ frontend/src/features/admin-form/builder/types.ts
@@ -9,6 +9,7 @@ export enum BuilderTabs {
 export type DragItem = {
   id: string
   fieldType: BasicField
+  isCreate?: boolean
 }
 
 export enum FieldDropType {
