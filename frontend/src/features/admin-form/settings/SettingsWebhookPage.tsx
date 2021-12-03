629baab2d1c9e89890392ef27a91455076e8b180
themogget <y.kaiyi@outlook.com>
2021-12-03 12:43:43 +0800

    feat(settings): style webhooks page

--- frontend/src/features/admin-form/settings/SettingsWebhookPage.tsx
+++ frontend/src/features/admin-form/settings/SettingsWebhookPage.tsx
@@ -7,15 +7,19 @@ export const SettingsWebhookPage =
   ({}: SettingsWebhookPageProps): JSX.Element => {
     return (
       <Box>
-        <Text mb="2.5rem" textStyle="h2">
+        <Text mb="2.5rem" textStyle="h2" color="secondary.500">
           Webhooks
         </Text>
-        <HStack>
-          <Text textStyle="subhead-1">Endpoint Url</Text>
-          <Text textStyle="body-2">(optional)</Text>
+        <HStack spacing="0.5rem">
+          <Text textStyle="subhead-1" color="secondary.700">
+            Endpoint Url
+          </Text>
+          <Text textStyle="body-2" color="neutral.700">
+            (optional)
+          </Text>
         </HStack>
         <Box width="42.4375rem">
-          <Text mb="0.75rem" textStyle="body-2">
+          <Text mb="0.75rem" textStyle="body-2" color="secondary.400">
             For developers and IT officers usage. We will POST encrypted form
             responses in real-time to the HTTPS endpoint specified here.
           </Text>
