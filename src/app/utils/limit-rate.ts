7f67b992d180735caa6c41bc3c73cf06b9223e2f
Timothee Groleau <timothee.groleau@gmail.com>
2022-02-15 09:31:06 +0800

    refactor(limit-rate): defaultOptions can a singular bootstrap-time object

--- src/app/utils/limit-rate.ts
+++ src/app/utils/limit-rate.ts
@@ -3,13 +3,32 @@ import RateLimit, {
   RateLimit as RateLimitFn,
 } from 'express-rate-limit'
 import { StatusCodes } from 'http-status-codes'
-import { merge } from 'lodash'
 
 import { createLoggerWithLabel } from '../config/logger'
 
 import { createReqMeta } from './request'
 
 const logger = createLoggerWithLabel(module)
+
+const defaultOptions: RateLimitOptions = {
+  windowMs: 60 * 1000, // Apply rate per-minute
+  max: 1200,
+  handler: (req, res) => {
+    logger.warn({
+      message: 'Rate limit exceeded',
+      meta: {
+        action: 'limitRate',
+        ...createReqMeta(req),
+        method: req.method,
+        rateLimitInfo: req.rateLimit,
+      },
+    })
+    return res.status(StatusCodes.TOO_MANY_REQUESTS).json({
+      message:
+        'We are experiencing a temporary issue. Please try again in one minute.',
+    })
+  },
+}
 
 /**
  * Returns a middleware which logs a message and returns 429 if the rate of requests
@@ -18,24 +37,5 @@ const logger = createLoggerWithLabel(module)
  * @return Rate-limiting middleware
  */
 export const limitRate = (options: RateLimitOptions = {}): RateLimitFn => {
-  const defaultOptions: RateLimitOptions = {
-    windowMs: 60 * 1000, // Apply rate per-minute
-    max: 1200,
-    handler: (req, res) => {
-      logger.warn({
-        message: 'Rate limit exceeded',
-        meta: {
-          action: 'limitRate',
-          ...createReqMeta(req),
-          method: req.method,
-          rateLimitInfo: req.rateLimit,
-        },
-      })
-      return res.status(StatusCodes.TOO_MANY_REQUESTS).json({
-        message:
-          'We are experiencing a temporary issue. Please try again in one minute.',
-      })
-    },
-  }
-  return RateLimit(merge(defaultOptions, options))
+  return RateLimit({ ...defaultOptions, ...options })
 }
