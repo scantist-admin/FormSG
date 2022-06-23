069c423bc67ce8a4fe43413e933bf085b2eaf4e5
Hans Tirtaputra <hansybastian@gmail.com>
2022-06-23 15:21:08 +0800

    fix: add key for mapped elements

--- frontend/src/features/workspace/WorkspacePage.tsx
+++ frontend/src/features/workspace/WorkspacePage.tsx
@@ -136,6 +136,7 @@ export const WorkspacePage = (): JSX.Element => {
           >
             {MOCK_WORKSPACES_DATA.map((workspace) => (
               <WorkspaceTab
+                key={workspace._id}
                 label={workspace.title}
                 numForms={workspace.numForms}
               />
@@ -144,7 +145,7 @@ export const WorkspacePage = (): JSX.Element => {
         </Flex>
         <TabPanels>
           {MOCK_WORKSPACES_DATA.map((workspace) => (
-            <TabPanel>
+            <TabPanel key={workspace._id}>
               <WorkspaceContent workspaceId={workspace._id} />
             </TabPanel>
           ))}
