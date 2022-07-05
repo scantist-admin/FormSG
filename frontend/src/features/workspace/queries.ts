f366a8c0e5e53c789d1c022ff84b5f1e4982c422
Hans Tirtaputra <hansybastian@gmail.com>
2022-07-05 14:49:18 +0800

    feat: use msw for GET workspaces instead of hardcoding in WorkspacePage

--- frontend/src/features/workspace/queries.ts
+++ frontend/src/features/workspace/queries.ts
@@ -1,20 +1,31 @@
 import { useQuery, UseQueryResult } from 'react-query'
 
 import { AdminDashboardFormMetaDto } from '~shared/types/form/form'
+import { AdminWorkspaceDto } from '~shared/types/workspace'
 
 import { ApiError } from '~typings/core'
 
-import { getDashboardView } from './WorkspaceService'
+import { getDashboardView, getWorkspacesView } from './WorkspaceService'
 
 const workspaceKeys = {
   all: ['workspace'] as const,
+  workspaces: ['workspaces'] as const,
 }
 
-export const useWorkspace = (): UseQueryResult<
+export const useDashboard = (): UseQueryResult<
   AdminDashboardFormMetaDto[],
   ApiError
 > => {
   return useQuery(workspaceKeys.all, () => getDashboardView(), {
+    staleTime: 5000,
+  })
+}
+
+export const useWorkspace = (): UseQueryResult<
+  AdminWorkspaceDto[],
+  ApiError
+> => {
+  return useQuery(workspaceKeys.workspaces, () => getWorkspacesView(), {
     staleTime: 5000,
   })
 }
