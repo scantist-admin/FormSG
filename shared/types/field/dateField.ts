1f25e2b2c7a5642c249c47eff48625428c7f70f5
Siddarth Nandanahosur Suresh <siddarthsuresh@Siddarths-MacBook-Pro.local>
2022-07-20 10:54:14 +0800

    refactor: refactor code as per comments

--- shared/types/field/dateField.ts
+++ shared/types/field/dateField.ts
@@ -7,19 +7,24 @@ export enum DateSelectedValidation {
   Custom = 'Custom date range',
 }
 
+export enum DaysOfTheWeek {
+  Sunday = 0,
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6,
+}
+
 export type DateValidationOptions = {
   customMaxDate: Date | null
   customMinDate: Date | null
   selectedDateValidation: DateSelectedValidation | null
 }
 
-export type RestrictParticularDaysOption = {
-  addParticularDayRestriction: boolean
-  invalidDaysOfTheWeek: number[]
-}
-
 export interface DateFieldBase extends MyInfoableFieldBase {
   fieldType: BasicField.Date
   dateValidation: DateValidationOptions
-  restrictParticularDays?: RestrictParticularDaysOption
+  invalidDaysOfTheWeek?: number[]
 }
